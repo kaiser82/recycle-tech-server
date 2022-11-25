41d330e8009ecdc5d12057b0e96bec23101638ed
kaiser82 <dr.kaiserahmed@gmail.com>
2022-11-25 18:13:47 +0600

    api for usr role

--- index.js
+++ index.js
@@ -129,6 +129,21 @@ async function run() {
             const user = await usersCollection.findOne(query);
             res.send({ isAdmin: user?.role === 'admin' });
         });
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.role === 'buyer' });
+        });
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
+        });
+
+
+
 
         app.post('/users', async (req, res) => {
             const user = req.body;
